fa603266616a1a18ba708c175696ad6439a65115
Firgo Bhaktiar Hamsah <71805833+igobhaktiar@users.noreply.github.com>
2021-04-28 22:58:00 +0700

    Minggu 7 - CRUD

---
+++ minggu7/crud_laravel/resources/views/backend/layouts/sidebar.blade.php
@@ -0,0 +1,30 @@
+<div id="sidebar" class="nav-collapse ">
+    <!-- sidebar menu start-->
+    <ul class="sidebar-menu">
+      <li class="">
+        <a class="" href="{{url('dashboard')}}">
+            <i class="icon_house_alt"></i>
+            <span>Dashboard</span>
+        </a>
+      </li>
+      <li>
+          <a class="" href="index.html">
+              <i class="icon_profile"></i>
+              <span>Profile</span>
+          </a>
+      </li>
+      <li class="sub-menu">
+          <a href="javascript:;" class="">
+              <i class="icon_document_alt"></i>
+              <span>Riwayat Hidup</span>
+              <span class="menu-arrow arrow_carrot-right"></span>
+          </a>
+          <ul class="sub">
+              <li><a href="{{url('pendidikan')}}">Pendidikan</a></li>
+              <li><a href="{{url('pengalaman_kerja')}}">Pengalaman Kerja</a></li>
+          </ul>
+      </li>
+
+    </ul>
+    <!-- sidebar menu end-->
+  </div>
